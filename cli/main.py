68ce89dc2618a5b1fde30eec1b755b47677a3131
Lshiroc <zeynalmerdanli4321@gmail.com>
2024-04-02 11:58:41 +0400

    --version command added

--- cli/main.py
+++ cli/main.py
@@ -5,6 +5,18 @@ from rich import print as rprint
 
 app = typer.Typer()
 
+def version_callback(value: bool):
+    if value:
+        typer.echo('CPM Version: v1.23')
+        raise typer.Exit()
+
+@app.callback()
+def main(
+    ctx: typer.Context,
+    version: bool = typer.Option(None, '--version', callback=version_callback),
+): 
+    pass
+
 @app.command("list")
 def sample_func():
     subprocess.run("dir", shell=True)
@@ -12,6 +24,7 @@ def sample_func():
 @app.command('hello')
 def sample_func():
     rprint('[red bold]Hi[/red bold] [yellow]World![/yellow]')
+
 
 if __name__ == '__main__':
     app()
